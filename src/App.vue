5ee8151bd1b95ea1c51584f94464f2ca7d69f418
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-01-24 14:16:28 -0500

    Start adding unsplash code

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 /* global CustomElement */
 <template>
   <div id="app" v-if="loaded">
-    <input :disabled="element.disabled" v-model="element.value" placeholder="edit me">
-    <p>Saved: {{ value }}</p>
-    <p>AK: {{ element.config.accessKey }}</p>
-    <p>SK: {{ element.config.secretKey }}</p>
+    <input :disabled="element.disabled" v-model="searchTerm" placeholder="edit me">
+    <button @click="searchPhotos">Search</button>
+    <p>Saved: {{ value }} {{ height }}</p>
+    <pre v-html="searchResults"></pre>
   </div>
 </template>
 
 <script>
+import Unsplash, { toJson } from 'unsplash-js'
+
 export default {
   name: 'app',
   data: function () {
     return {
       loaded: false,
+      searchTerm: "",
+      searchResults: "",
+      height: "",
       element: {},
       configuration: {}
     }
@@ -21,6 +26,38 @@ export default {
   computed: {
     value() {
       return this.element.value
+    },
+    unsplashInstance() {
+      return this.loaded
+        ? new Unsplash({
+            applicationId: this.element.config.accessKey,
+            secret: this.element.config.secretKey
+          })
+        : null
+    }
+  },
+  methods: {
+    searchPhotos() {
+      if(this.unsplashInstance && this.searchTerm) {
+        this.unsplashInstance.search.photos(this.searchTerm)
+        .then(toJson)
+        .then(json => {
+          this.searchResults = json
+        })
+      }
+    },
+    updateSize() {
+      // Update the custom element height in the Kentico UI.
+      const height = Math.max(
+        document.body.scrollHeight,
+        document.body.offsetHeight,
+        document.documentElement.clientHeight,
+        document.documentElement.scrollHeight,
+        document.documentElement.offsetHeight
+      )
+
+      this.height = height
+      CustomElement.setHeight(height);
     }
   },
   watch: {
@@ -32,15 +69,19 @@ export default {
   },
   created: function() {
     CustomElement.init((element, context) => {
-    // Set up the element with initial value
-    this.element = element
-    this.context = context
-    this.loaded = true
-    // console.log(element)
-    // console.log(context)
-})
+      // Set up the element with initial value
+      this.element = element
+      this.context = context
+      this.loaded = true
+      this.updateSize()
+    })
+  },
+  updated: function() {
+    this.updateSize()
   }
 }
+
+
 </script>
 
 <style>
